cca4951b36d49f94ebcd218849310e8627d9e148
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-27 17:25:01 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -16,6 +16,7 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.strip
+      self.call unless input == "exit"
   end
   
 end
